35717827b71f55c6a8be0096b551cd0c0843eb44
LauraP92 <panalauracristina@outlook.com>
2019-10-27 11:25:22 +0000

    modify-application

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ window.addEventListener("load", () => {
     let locationTimezone = document.querySelector(".location-timezone");
     let temperatureSection = document.querySelector(".temperature");
     const temperatureSpan = document.querySelector(".temperature span");
-    
+    let darkSkybut = document.querySelector("#darkSky");
+    let openWeatherbut = document.querySelector("#openWeather");
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -15,34 +17,65 @@ window.addEventListener("load", () => {
 
             const proxy = `https://cors-anywhere.herokuapp.com/`;
             const api = `${proxy}https://api.darksky.net/forecast/a3f6e105434bb120691e7e8ef4cf6303/${lat},${long}`;
-            
-            
-            fetch(api)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                const { temperature, summary, icon } = data.currently;
-                // Set DOM Elem from the API
-                temperatureDegree.textContent = temperature;
-                temperatureDescription.textContent = summary;
-                locationTimezone.textContent = data.timezone;
-                // Formula for Celsius
-                let celsius = (temperature - 32) * (5 / 9);
-                // Set Icon
-                setIcons(icon, document.querySelector(".icon"));
+            const api2 = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=b0dd24d4643315fefa7f09ed84a843ec`;
 
-                // Change temp to Celsius/Farenheit
-                temperatureSection.addEventListener("click", () => {
-                    if (temperatureSpan.textContent === "F") {
-                        temperatureSpan.textContent = "C";
-                        temperatureDegree.textContent = Math.floor(celsius);
-                    } else {
-                        temperatureSpan.textContent = "F";
+            openWeatherbut.addEventListener("click", () => {
+                fetch(api2)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log(data);
+                        // Set DOM Elem from the API
+                        temperatureDegree.textContent = data.main.temp;
+                        temperatureDescription.textContent = data.weather[0].description;
+                        locationTimezone.textContent = data.sys.country;
+                        // Formula for Celsius
+                        let celsius = (data.main.temp - 32) * (5 / 9);
+                        // Set Icon
+                        setIcons(data.weather[0].icon, document.querySelector(".icon"));
+
+                        // Change temp to Celsius/Farenheit
+                        temperatureSection.addEventListener("click", () => {
+                            if (temperatureSpan.textContent === "F") {
+                                temperatureSpan.textContent = "C";
+                                temperatureDegree.textContent = Math.floor(celsius);
+                            } else {
+                                temperatureSpan.textContent = "F";
+                                temperatureDegree.textContent = data.main.temp;
+                            }
+                        })
+                    })
+            })
+
+            darkSkybut.addEventListener("click", () => {
+                fetch(api)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log(data);
+                        const { temperature, summary, icon } = data.currently;
+                        // Set DOM Elem from the API
                         temperatureDegree.textContent = temperature;
-                    }
-                })
+                        temperatureDescription.textContent = summary;
+                        locationTimezone.textContent = data.timezone;
+                        // Formula for Celsius
+                        let celsius1 = (temperature - 32) * (5 / 9);
+                        // Set Icon
+                        setIcons(icon, document.querySelector(".icon"));
+
+                        // Change temp to Celsius/Farenheit
+                        temperatureSection.addEventListener("click", () => {
+                            if (temperatureSpan.textContent === "F") {
+                                temperatureSpan.textContent = "C";
+                                temperatureDegree.textContent = Math.floor(celsius1);
+                            } else {
+                                temperatureSpan.textContent = "F";
+                                temperatureDegree.textContent = temperature;
+                            }
+                        })
+                    })
             });
         });
     }
